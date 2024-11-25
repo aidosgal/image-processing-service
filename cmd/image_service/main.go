09a64083f51a33cb62509215244849943fe9b8af
aidosgal <agalimzhan928@gmail.com>
2024-11-25 21:09:04 +0500

    feat: setup the logger

--- cmd/image_service/main.go
+++ cmd/image_service/main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/aidosgal/image-processing-service/internal/config"
+)
+
+const (
+    envLocal = "local"
+    envDev = "dev"
+    envProd = "prod"
 )
 
 func main() {
     cfg := config.MustLoad()
 
-    fmt.Println(cfg)
+    log := setupLogger(cfg.Env)
+
+    log.Info("Just test")
+}
+
+func setupLogger(env string) *slog.Logger {
+    var log *slog.Logger
+    switch env {
+    case envLocal:
+        log = slog.New(
+            slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envDev:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envProd:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+        )
+    }
+
+    return log
 }
